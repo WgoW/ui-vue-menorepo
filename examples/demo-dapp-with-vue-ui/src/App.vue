5c19655336b551682016b75c088063a7ec233472
a <a@gmail.com>
2024-06-22 20:48:00 +0800

    :sparkles: 使用@tonconnect/ui-vue连接钱包

---
+++ examples/demo-dapp-with-vue-ui/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import {useProvide} from "tonconnect-ui-vue";
+const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'
+
+console.log('开始调用 useProvide', manifestUrl)
+useProvide({
+  manifestUrl,
+})
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+</style>
